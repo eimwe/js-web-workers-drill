42ba02a0cf8ed47c5bddc4c84b3298930de80cd5
eimwe <prokopenkoserge@inbox.ru>
2024-01-29 14:21:27 +0600

    Replaced grayscale filter with 100 itrs of lighten up

--- src/scripts/workers/imageProcessor.js
+++ src/scripts/workers/imageProcessor.js
@@ -1,14 +1,16 @@
 import createWorker from "../workers/helper.js";
 
 const imageProcessorWorker = createWorker(() => {
-  //Grayscale filter
   self.onmessage = (event) => {
     const imageData = event.data;
     const data = imageData.data;
 
-    for (let i = 0; i < data.length; i += 4) {
-      const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-      data[i] = data[i + 1] = data[i + 2] = avg;
+    for (let iteration = 0; iteration < 100; iteration++) {
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] = Math.min(data[i] + 1, 255); // Increase red component
+        data[i + 1] = Math.min(data[i + 1] + 1, 255); // Increase green component
+        data[i + 2] = Math.min(data[i + 2] + 1, 255); // Increase blue component
+      }
     }
 
     self.postMessage(imageData);
