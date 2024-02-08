ec20972fe8607f7872e3c2045912fff78f4dda02
eimwe <prokopenkoserge@inbox.ru>
2024-02-08 15:20:56 +0600

    Deleted previous web worker files

--- src/scripts/workers/helper.js
+++
@@ -1,8 +0,0 @@
-const createWorker = (workerFunction) => {
-  const blob = new Blob([`(${workerFunction.toString()})()`], {
-    type: "application/javascript",
-  });
-  return new Worker(URL.createObjectURL(blob));
-};
-
-export default createWorker;
